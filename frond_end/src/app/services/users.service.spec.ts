e346862f77b56528fa98bbcc6ef6e68f3bdaa41e
Ndeye Bougouma Sy <mamys8931@gmail.com>
2023-01-18 11:02:32 +0100

    profil

--- frond_end/src/app/services/users.service.spec.ts
+++ frond_end/src/app/services/users.service.spec.ts
@@ -2,11 +2,6 @@ import { TestBed } from '@angular/core/testing';
 
 import { UsersService } from './users.service';
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 7542b6dc02078a40ce190e82ffca5ab088a5a949
 describe('UsersService', () => {
   let service: UsersService;
 
