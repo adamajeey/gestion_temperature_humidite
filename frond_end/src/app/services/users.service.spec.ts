a01ef8d02fc1460bdd9838db9e42dc18ca1c03b9
papesoukad96 <papesoukad@gmail.com>
2023-01-25 18:53:02 +0100

    tgghgh

--- frond_end/src/app/services/users.service.spec.ts
+++ frond_end/src/app/services/users.service.spec.ts
@@ -2,11 +2,6 @@ import { TestBed } from '@angular/core/testing';
 
 import { UsersService } from './users.service';
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> e49cae5b0247a1c5f83a50b40bbed06b7fbec124
 describe('UsersService', () => {
   let service: UsersService;
 
